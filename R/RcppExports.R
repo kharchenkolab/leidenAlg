730ffe825cc91a2d163225014e00e9be9db37c36
evanbiederstedt <evan.biederstedt@gmail.com>
2020-08-16 20:03:05 -0400

    update doc

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -5,6 +5,20 @@ checkOpenMP <- function() {
     .Call('_leidenAlg_checkOpenMP', PACKAGE = 'leidenAlg')
 }
 
+#' Finds the optimal partition using the Leiden algorithm
+#' 
+#' @param graph The igraph graph to define the partition on
+#' @param edge_weights Vector of edge weights. In weighted graphs, a real number is assigned to each (directed or undirected) edge. Refer to igraph, weighted graphs.
+#' @param resolution Integer resoluiton parameter controlling communities detected (default=1.0) Higher resolutions lead to more communities, while lower resolutions lead to fewer communities.
+#' @param niter Number of iterations that the algorithm should be run for (default=2)
+#' @return A vector of membership values
+#' @examples 
+#' library(igraph)
+#' g <- make_star(10)
+#' E(g)$weight <- seq(ecount(g))
+#' find_partition(g, E(g)$weight)
+#' 
+#' @export
 find_partition <- function(graph, edge_weights, resolution = 1.0, niter = 2L) {
     .Call('_leidenAlg_find_partition', PACKAGE = 'leidenAlg', graph, edge_weights, resolution, niter)
 }
